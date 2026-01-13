7b642d865b4baebfa8645ce3f278f134084b6419
diepn <54232678+nndiep@users.noreply.github.com>
2026-01-13 15:24:33 +0700

    fix: install expo-asset and configure hashAssetFiles to fix critical build error

--- hpws-mobile/metro.config.js
+++ hpws-mobile/metro.config.js
@@ -4,6 +4,9 @@ const config = {
     resolver: {
         assetExts: [...getDefaultConfig(__dirname).resolver.assetExts, 'tflite', 'mp3'],
     },
+    transformer: {
+        assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+    },
 };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
